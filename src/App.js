8652907c3aaee37f368d912ba42344cc13649f00
teeoris <ayodeleorisatuyi@gmail.com>
2023-03-19 01:13:43 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
+import { data } from './data';
+import './app.css';
 
 function App() {
+const [footballers, setFootballers] = useState(data);
+
+const removeFootballer = (id)=> {
+  const newFootballer = footballers.filter((footballer)=> footballer.id !== id);
+  setFootballers(newFootballer);
+};
+
+const clearAll = ()=> {
+  setFootballers([])
+};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='footballer'>
+      <div className='footballer__title'>
+        <h1>Man Utd Players</h1>
+        <span></span>
+      </div>
+      {footballers.map((footballer)=> {
+        const {id, name, position} = footballer;
+        return (
+          <section key={id} className='footballer__container' >
+            <div className='footballer__info'>
+              <h2>{name}</h2>
+              <p>{position}</p>
+              <button className='btn' type='button' onClick={()=> removeFootballer(id)}>Remove</button>
+              <hr />
+            </div>
+          </section>
+        )
+      })}
+      <button className='btn' type='button' onClick={clearAll}>Clear all</button>
     </div>
   );
 }
---
+++ src/data.jsx
@@ -0,0 +1,6 @@
+export const data = [
+    {id:"1", name:"Marcos Rashford", club: "Man Utd", position: "Striker"},
+    {id:"2", name: "Bruno Fernandes", club: "Man Utd", position: "Mildfilder"},
+    {id:"3", name: "Casemero", club: "Man Utd", position: "Defensive Mildfilder"},
+    {id:"4", name: "David Degea", club: "Man Utd", position: "Goal Keeper"},
+];
